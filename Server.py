0e4a4602fe913ecb2909ea50799594dcb86fdb8e
Shreyas <shreyasjoshi97@hotmail.com>
2018-11-21 19:49:53 +0000

    Reverted back to socket format

--- Server.py
+++ Server.py
@@ -1,19 +1,46 @@
-from flask import Flask
+import socket
 import os
+from _thread import *
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return "Hi"
-
-
-@app.route("/", methods=["POST"])
-def send_data():
-    return "Message received!"
+host = '0.0.0.0'
+port = os.environ.get("PORT", 5000)
+port = int(port)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get('Port', 5000))
-    app.run(host='0.0.0.0', port=port)
+try:
+    s.bind((host, port))
+except socket.error as e:
+    print(str(e))
+
+s.listen(5)
+print('Waiting for connection')
+
+
+def threaded_client(conn):
+    conn.send(str.encode('Welcome, type your info\n'))
+    dataHolder = ''
+    while True:
+        try:
+            data = conn.recv(1024)
+            dataHolder = dataHolder + data.decode('utf-8')
+            for string in dataHolder:
+                if string == '\n':
+                    reply = "HTTP/1.1 200 OK\n" + "Content-Type: text\n" + "\n" + dataHolder
+                    conn.sendall(str.encode(reply))
+                    serverOutput = addr[0] + ': ' + dataHolder
+                    dataHolder = ''
+                    print(serverOutput)
+                    if not data:
+                        break
+        except BrokenPipeError as e:
+            print("Socket error: ", e)
+        except ConnectionResetError as e:
+            print("Connection reset error")
+    conn.close()
+
+
+while True:
+    conn, addr = s.accept()
+    print('Connected to: ' + addr[0] + ':' + str(addr[1]))
+    start_new_thread(threaded_client, (conn,))
