d34aa82e2be45299063103b084c86f6eb13377b3
Shreyas <shreyasjoshi97@hotmail.com>
2018-11-24 10:48:43 +0000

    Using endoftransmission to establish when the whole message ends

--- Server.py
+++ Server.py
@@ -27,7 +27,7 @@ def threaded_client(conn):
             data_holder = data_holder + data.decode('utf-8')
             for string in data_holder:
                 print(data_holder)
-                if string == '\n':
+                if string == "endoftransmission":
                     print("Newline found" + data_holder)
                     reply = "HTTP/1.1 200 OK\n" + "Content-Type: text/html\n" + "\n" + data_holder
                     # reply = data_holder
