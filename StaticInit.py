4871a59c256ed4e6d6c7598d6f48a05a77d5fea7
shreyasjoshi97 <shreyasjoshi97@hotmail.com>
2019-02-07 18:24:46 +0000

    Removed redundant class

--- StaticInit.py
+++ StaticInit.py
@@ -1,15 +1,18 @@
-import StaticAnalysis
 import numpy as np
-
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
 
 class StaticInit:
+    data = ""
+    model = ""
+
     def __init__(self):
         self.permissions = {}
         self.results = {}
         self.initialise_permissions()
         self.file = open("permissions.txt", "r")
-        self.static_analysis = StaticAnalysis.StaticAnalysis()
-        self.static_analysis.init_model()
+        self.init_model()
         self.initialise_results()
 
     def initialise_permissions(self):
@@ -28,8 +31,31 @@ class StaticInit:
                     self.permissions.update({permission: 1})
             prediction = list(self.permissions.values())
             prediction_array = np.array(prediction).reshape(1, -1)
-            result = self.static_analysis.make_prediction(prediction_array)
+            result = self.make_prediction(prediction_array)
             self.results.update({name: result[0]})
             self.initialise_permissions()
         print(self.results)
         return self.results
+
+    def init_model(self):
+        self.data = pd.read_csv("train.csv", sep=';')
+        array = self.data.values
+        X = array[:, 0:330]
+        Y = array[:, 330]
+        validation_size = 0.30
+        seed = 10
+        X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size,
+                                                                                        random_state=seed)
+
+        # test = pd.read_csv("test.csv", sep=';')
+        # test_values = test.values;
+        # X_test = test_values[:, 0:330]
+        # Y_test = test_values[:, 330]
+
+        self.model =LogisticRegression(solver='lbfgs')
+        self.model.fit(X_train, Y_train)
+
+    def make_prediction(self, predictor):
+        pred = self.model.predict(predictor)
+        return pred
+
