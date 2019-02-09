1f334d6c059d0fd7263a0be2cb14ecc80bc4bb87
shreyasjoshi97 <shreyasjoshi97@hotmail.com>
2019-02-09 20:43:19 +0000

    Bug fix on threading

--- Server.py
+++ Server.py
@@ -56,7 +56,7 @@ s.listen(1)
 print('Server listening')
 
 
-def threaded_client():
+def threaded_client(conn):
     data_holder = ''
     sending = True
     while sending:
@@ -91,7 +91,7 @@ while True:
     print('Connected to: ' + addr[0] + ':' + str(addr[1]))
     if os.path.exists(permissions_file):
         os.remove(permissions_file)
-    t = threading.Thread(target=threaded_client)
+    t = threading.Thread(target=threaded_client, args=(conn,))
     t.start()
     t.join()
     # start_new_thread(threaded_client, (conn,))
