5fc58a625c4ed40f6b815b130d0f752237b47120
shreyasjoshi97 <shreyasjoshi97@hotmail.com>
2019-02-09 18:09:40 +0000

    Revert "Use replace to insert newlines"
    This reverts commit 0ce0cf114a70a94c33537cc80fa60795a0e4159f.

--- Server.py
+++ Server.py
@@ -20,10 +20,12 @@ def init_file(name):
 def parse_data(message):
     f = None
     start_reading = False
-    message = message.replace("^", "\n")
     for x in message:
         if start_reading:
-            f.write(x)
+            if x == '^':
+                f.write('\n')
+            else:
+                f.write(x)
         if x == "|":
             start_reading = True
             f = init_file(permissions_file)
