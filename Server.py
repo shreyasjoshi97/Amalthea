ef0a6e35c03d7bc8a086159f9553eb603b4fc48d
shreyasjoshi97 <shreyasjoshi97@hotmail.com>
2019-02-09 18:52:52 +0000

    Revert to stable

--- Server.py
+++ Server.py
@@ -20,14 +20,10 @@ def init_file(name):
 
 
 def parse_data(message):
-    f = None
     start_reading = False
     for x in message:
         if start_reading:
-            if x == '^':
-                f.write('\n')
-            else:
-                f.write(x)
+            f.write(x)
         if x == "|":
             start_reading = True
             f = init_file(permissions_file)
