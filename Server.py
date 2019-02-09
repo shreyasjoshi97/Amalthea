add674686756788fbc0f9a91025aebf885c34633
Shreyas Joshi <shreyasjoshi97@hotmail.com>
2019-02-09 00:03:52 +0000

    Bug fix v3

--- Server.py
+++ Server.py
@@ -28,7 +28,7 @@ def threaded_client(conn):
             for string in data_holder:
                 if string == '\n':
                     print("Newline found" + data_holder)
-                    ret = "Result:" + string
+                    ret = "Result:" + data_holder
                     reply = "HTTP/1.1 200 OK\n" + "Content-Type: text/html\n" + "\n" + ret + "\n"
                     # reply = data_holder
                     conn.sendall(str.encode(reply))
