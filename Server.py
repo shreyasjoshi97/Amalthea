d996727ea22bff6759b343a95a570a4dccff58cc
shreyasjoshi97 <shreyasjoshi97@hotmail.com>
2019-02-09 14:10:49 +0000

    Implemeting static analysis model

--- Server.py
+++ Server.py
@@ -1,12 +1,45 @@
 import socket
 import os
 import re
+import StaticInit
 from _thread import *
 
 host = '0.0.0.0'
 port = os.environ.get("PORT", 5000)
 port = int(port)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+permissions_file = 'permissions.txt'
+behaviour_file = 'behaviour.csv'
+
+
+def init_file(name):
+    file = open(name, "a")
+    return file
+
+
+def parse_data(message):
+    f = ''
+    start_reading = False
+    data = ''
+    for x in message:
+        if start_reading:
+            data += x
+        if x == "|":
+            start_reading = True
+            f = init_file(permissions_file)
+    f.write(data)
+    result = setup_analysis()
+    return result
+
+
+def setup_analysis():
+    results = ''
+    if os.path.exists(permissions_file):
+        static = StaticInit.StaticInit()
+        results = static.initialise_results()
+    # if os.path.exists(behaviour_file):
+        # behaviour = BehaviourInit.BehaviourInit()
+    return results
 
 
 try:
@@ -18,17 +51,6 @@ s.listen(1)
 print('Server listening')
 
 
-def parse_data(message):
-    start_reading = False
-    data = ''
-    for x in message:
-        if start_reading:
-            data += x
-        if x == "|":
-            start_reading = True
-    return data
-
-
 def threaded_client(conn):
     data_holder = ''
     sending = True
@@ -38,7 +60,7 @@ def threaded_client(conn):
             data_holder = data_holder + data.decode('utf-8')
             for string in data_holder:
                 if string == '\n':
-                    ret1 = parse_data(data_holder)
+                    ret1 = "Result Start " + parse_data(data_holder) + " Result End"
                     print(ret1)
                     ret = data_holder + "\n" + ret1
                     reply = "HTTP/1.1 200 OK\n" + "Content-Type: text/html\n" + "\n" + ret + "\n\n"
---
+++ StaticInit.py
@@ -0,0 +1,61 @@
+import numpy as np
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+
+class StaticInit:
+    data = ""
+    model = ""
+    file = ''
+
+    def __init__(self):
+        self.permissions = {}
+        self.results = {}
+        self.initialise_permissions()
+        self.init_model()
+
+    def initialise_permissions(self):
+        p = open("permissiontypes.txt")
+        for x in p:
+            name = x[:-1]
+            self.permissions.update({name: 0})
+
+    def initialise_results(self):
+        self.file = open("permissions.txt", "r")
+        for x in self.file:
+            line = x.split(',')
+            name = line[0]
+            line.pop(0)
+            for permission in line:
+                if permission in self.permissions:
+                    self.permissions.update({permission: 1})
+            prediction = list(self.permissions.values())
+            prediction_array = np.array(prediction).reshape(1, -1)
+            result = self.make_prediction(prediction_array)
+            self.results.update({name: result[0]})
+            self.initialise_permissions()
+        print(self.results)
+        return self.results
+
+    def init_model(self):
+        self.data = pd.read_csv("train.csv", sep=';')
+        array = self.data.values
+        X = array[:, 0:330]
+        Y = array[:, 330]
+        validation_size = 0.30
+        seed = 10
+        X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size,
+                                                                                        random_state=seed)
+
+        # test = pd.read_csv("test.csv", sep=';')
+        # test_values = test.values;
+        # X_test = test_values[:, 0:330]
+        # Y_test = test_values[:, 330]
+
+        self.model =LogisticRegression(solver='lbfgs')
+        self.model.fit(X_train, Y_train)
+
+    def make_prediction(self, predictor):
+        pred = self.model.predict(predictor)
+        return pred
+
