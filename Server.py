1a55040d1220752619768469703b7f8e7bc737a0
shreyasjoshi97 <shreyasjoshi97@hotmail.com>
2019-02-10 15:23:36 +0000

    Revert "Revert "Increase receive buffer size to 8192""
    This reverts commit 28d15b71bb40fb6b3450c9cb55e993f4bab7fb53.

--- Server.py
+++ Server.py
@@ -80,8 +80,7 @@ def threaded_client(conn):
                     ret = data_holder + "\n" + ret1
                     reply = "HTTP/1.1 200 OK\n" + "Content-Type: text/html\n" + "\n" + ret + "\n\n"
                     # reply = data_holder
-                    if ret1 != "Result Start {} Result End":
-                        conn.sendall(str.encode(reply))
+                    conn.sendall(str.encode(reply))
                     #print(ret)
                     if not data:
                         print("No data received")
