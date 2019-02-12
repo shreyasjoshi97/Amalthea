89cc17388c1e9fb4a0e405efd8e91936a072896d
shreyasjoshi97 <shreyasjoshi97@hotmail.com>
2019-02-12 17:55:44 +0000

    Revert

--- Server.py
+++ Server.py
@@ -101,7 +101,7 @@ def threaded_client(conn):
 while True:
     conn, addr = s.accept()
     print('Connected to: ' + addr[0] + ':' + str(addr[1]))
-    # t = threading.Thread(target=threaded_client, args=(conn,))
-    # t.start()
-    # t.join()
+    #t = threading.Thread(target=threaded_client, args=(conn,))
+    #t.start()
+    #t.join()
     start_new_thread(threaded_client, (conn,))
