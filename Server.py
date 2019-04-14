e9b2d02b56bb777f959d32e550d243287f80591a
shreyasjoshi97 <shreyasjoshi97@hotmail.com>
2019-04-14 21:35:38 +0100

    Post Testing

--- Server.py
+++ Server.py
@@ -6,9 +6,9 @@ import DeltaCalculator
 import AverageCalculator
 import BehaviourChange
 from _thread import *
-
+#374
 host = ''
-port = os.environ.get("PORT", 7000)
+port = os.environ.get("PORT", 50000)
 port = int(port)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -18,32 +18,34 @@ behaviour_file = 'behaviour.csv'
 averages_file = 'averages.csv'
 
 
-def init_file(message):
+def delete_files():
     if os.path.exists(permissions_file):
         os.remove(permissions_file)
-    if os.path.exists(behaviour_file):
-        os.remove(behaviour_file)
     if os.path.exists(averages_file):
         os.remove(averages_file)
-    if os.path.exists(delta_file):
-        os.remove(delta_file)
+    if os.path.exists(behaviour_file):
+        os.remove(behaviour_file)
+
+
+def init_file(message):
+    delete_files()
 
     if "|" in message:
         print("Permissions process")
         file = open(permissions_file, "a")
         return file
-    elif "^" in message:
-        print("Delta process")
-        file = open(delta_file, "a")
-        file.write("\"Name\",\"CPU\",\"VSS\",\"RSS\",\"PCY\",\"Time\"\n")
-        return file
     elif "+" in message:
         print("Average process")
         file = open(averages_file, "a")
-        file.write("\"Name\",\"active_cpu_q1\",\"active_cpu_q2\",\"active_cpu_q3\","
-                   "\"idle_cpu_q1\",\"idle_cpu_q2\",\"idle_cpu_q3\","
-                   "\"active_mem_q1\",\"active_mem_q2\",\"active_mem_q3\","
-                   "\"idle_mem_q1\",\"idle_mem_q2\",\"idle_mem_q3\"\n")
+        file.write("\"Name\",\"CPU\",\"VSS\",\"RSS\",\"PCY\",\"Time\"\n")
+        return file
+    # elif "+" in message:
+    #     print("Average process")
+    #     file = open(averages_file, "a")
+    #     file.write("\"Name\",\"active_cpu_q1\",\"active_cpu_q2\",\"active_cpu_q3\","
+    #                "\"idle_cpu_q1\",\"idle_cpu_q2\",\"idle_cpu_q3\","
+    #                "\"active_mem_q1\",\"active_mem_q2\",\"active_mem_q3\","
+    #                "\"idle_mem_q1\",\"idle_mem_q2\",\"idle_mem_q3\"\n")
         return file
     elif "~" in message:
         print("Behaviour process")
@@ -81,15 +83,16 @@ def setup_analysis():
         print("HERE")
         static = StaticAnalysis.StaticAnalysis()
         results = static.initialise_results()
-    if os.path.exists(delta_file):
-        delta = DeltaCalculator.DeltaCalculator()
-        results = delta.begin_analysis()
+    # if os.path.exists(delta_file):
+    #     delta = DeltaCalculator.DeltaCalculator()
+    #     results = delta.begin_analysis()
     if os.path.exists(averages_file):
         average = AverageCalculator.AverageCalculator()
         results = average.begin_analysis()
     if os.path.exists(behaviour_file):
         behaviour = BehaviourChange.BehaviourChange()
         results = behaviour.begin_analysis()
+    delete_files()
     return str(results)
 
 
